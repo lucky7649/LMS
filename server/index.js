db129e332e5e07652f694fdbd1781fa411f0f4e0
Lucky7649 <luckypaswan745@gmail.com>
2025-11-20 00:41:27 +0530

    fix code

--- server/index.js
+++ server/index.js
@@ -28,8 +28,13 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://lms-five-swart-45.vercel.app"
+    ],
     credentials: true,
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 

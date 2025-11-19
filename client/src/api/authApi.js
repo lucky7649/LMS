df3726c234aa1504b6231f22edfe0755c178423c
Lucky7649 <luckypaswan745@gmail.com>
2025-11-20 00:23:58 +0530

    fix code

--- client/src/api/authApi.js
+++ client/src/api/authApi.js
@@ -1,7 +1,7 @@
 import { userLoggedIn, userLoggedOut } from "@/features/authSlice";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const USER_API = "http://localhost:8000/api/v1/user";
+const USER_API = "https://lms-u5w9.onrender.com/api/v1/user";
 
 export const authApi = createApi({
   reducerPath: "authApi",
--- client/src/api/purchaseApi.js
+++ client/src/api/purchaseApi.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const COURSE_PURCHASE_API = "http://localhost:8000/api/v1/purchase";
+const COURSE_PURCHASE_API = "https://lms-u5w9.onrender.com/api/v1/purchase";
 
 export const purchaseApi = createApi({
   reducerPath: "purchaseApi",
